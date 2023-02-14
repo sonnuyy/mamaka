980b0fb800fe605667f65a50d5bcdf7a8b69d1f9
Sonya <makakak846@gmail.com>
2023-02-15 00:10:25 +0500

    vvv

---
+++ script.js
@@ -0,0 +1,23 @@
+const dino = document.getE1ementById("dino");
+const cactus = document.getE1ementById("cactus");
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+
+function jump () {
+    if (dino.classList !== "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout( function() {
+        dino.classList.remove("jump")
+    }, 300)
+}
+setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if (cactusLeft < 50 &&  cactusLeft > 0 && dinoTop >= 140) {
+        alert("GAME OVER!!")
+    }
+}, 10);
